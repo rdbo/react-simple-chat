da1d6a387de9a07ee7b5bcdb67dfe32418bad371
rdbo <rdbodev@gmail.com>
2024-04-26 13:05:27 -0300

    added websocket and connection status on client

--- src/Chat.tsx
+++ src/Chat.tsx
@@ -1,21 +1,40 @@
 import { KeyboardEvent, useState } from "react";
 import { Textarea } from "./components/ui/textarea";
+import useWebSocket, { ReadyState } from "react-use-websocket";
 
 export default function Chat() {
   const [message, setMessage] = useState("");
-  const webSocketUrl = (window.location.protocol == "https:" ? "wss://" : "ws://") + window.location.host + "/api/websocket";
+  const webSocketUrl =
+    (window.location.protocol == "https:" ? "wss://" : "ws://") +
+    window.location.host +
+    "/api/websocket";
+  const { sendMessage, lastMessage, readyState } = useWebSocket(webSocketUrl);
 
-  const sendMessage = () => {
-    setMessage("");
+  let connectionStatus: "connected" | "connecting" | "disconnected";
+  if (readyState == ReadyState.OPEN) {
+    connectionStatus = "connected";
+  } else if (readyState == ReadyState.CONNECTING) {
+    connectionStatus = "connecting";
+  } else {
+    connectionStatus = "disconnected";
   }
+  const connectionColor = {
+    connected: "text-green-500",
+    connecting: "text-yellow-500",
+    disconnected: "text-red-500",
+  };
+
+  const sendMessageToServer = () => {
+    setMessage("");
+  };
 
   const handleKeyDown = (event: KeyboardEvent) => {
     // TODO: Grow/shrink textarea height
     if (event.key == "Enter" && !event.shiftKey) {
       event.preventDefault();
-      sendMessage();
+      sendMessageToServer();
     }
-  }
+  };
 
   const updateMessage = (newMessage: string) => {
     setMessage(newMessage);
@@ -24,8 +43,18 @@ export default function Chat() {
   return (
     <div className="flex flex-col mt-2 grow mx-8 text-lg">
       <h1 className="font-bold text-xl text-center">Messages</h1>
-      <div className="border dark:border-zinc-800 rounded-lg my-2 grow px-2 py-1">
-        <p>hello</p>
+      <div className="border dark:border-zinc-800 rounded-lg my-2 grow">
+        <div className="px-2 py-1 border-b border-zinc-800">
+          <p>
+            Status:{" "}
+            <span className={connectionColor[connectionStatus] + " font-bold"}>
+              {connectionStatus}
+            </span>
+          </p>
+        </div>
+        <div className="px-2 py-1">
+          <p>hello</p>
+        </div>
       </div>
       <div className="pb-8 flex items-center">
         <Textarea
@@ -35,7 +64,10 @@ export default function Chat() {
           onKeyDown={handleKeyDown}
           onChange={(event) => updateMessage(event.target.value)}
         />
-        <button className={"ml-2 rounded-full" + (message ? "" : " opacity-50")} disabled={!message}>
+        <button
+          className={"ml-2 rounded-full" + (message ? "" : " opacity-50")}
+          disabled={!message}
+        >
           <img src="/assets/images/send-message.png" className="h-12" />
         </button>
       </div>
