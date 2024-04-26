48940a2656533186d2768e20aee094912eee662c
rdbo <rdbodev@gmail.com>
2024-04-25 21:24:46 -0300

    added way to remove nickname

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -4,11 +4,20 @@ import { useContext } from "react";
 export default function NavBar() {
   const { nickname, setNickname } = useContext(Nickname) as NicknameData;
 
+  const clearNickname = () => setNickname("");
+
   return (
     <div className="px-4 py-2 border flex justify-between items-center bg-white">
       <h1 className="font-bold text-2xl">react-simple-chat</h1>
       <div>
-        <p>{ nickname && nickname || "Join" }</p>
+        {nickname ? (
+          <button onClick={clearNickname} className="flex items-center font-bold">
+            <img src="/assets/images/user.png" className="h-8 mr-1" />
+            <p>{nickname}</p>
+          </button>
+        ) : (
+          ""
+        )}
       </div>
     </div>
   );
