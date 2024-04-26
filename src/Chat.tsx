72a665d94e0588a7599f7343bc23ad2b7f629850
rdbo <rdbodev@gmail.com>
2024-04-26 08:03:54 -0300

    improved ui

--- src/Chat.tsx
+++ src/Chat.tsx
@@ -5,7 +5,7 @@ export default function Chat() {
   return (
     <div className="flex flex-col mt-2 grow mx-8 text-lg">
       <h1 className="font-bold text-xl text-center">Messages</h1>
-      <div className="border rounded-lg my-2 grow px-2 py-1">
+      <div className="border dark:border-zinc-800 rounded-lg my-2 grow px-2 py-1">
         <p>hello</p>
       </div>
       <div className="pb-8 flex items-center">
