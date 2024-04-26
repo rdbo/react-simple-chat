ac77578abecdb461c95b6a7ffec1982c0bbc0db6
rdbo <rdbodev@gmail.com>
2024-04-26 08:13:13 -0300

    added theme switcher

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,23 +1,58 @@
 import { Nickname, NicknameData } from "@/App";
 import { useContext } from "react";
+import { useTheme } from "./theme-provider";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "./ui/dropdown-menu";
+import { Button } from "./ui/button";
+import { Moon, Sun } from "lucide-react";
 
 export default function NavBar() {
   const { nickname, setNickname } = useContext(Nickname) as NicknameData;
+  const { setTheme } = useTheme();
 
   const clearNickname = () => setNickname("");
 
   return (
     <div className="px-4 py-2 border-b flex justify-between items-center bg-white dark:bg-zinc-950 dark:text-white dark:border-zinc-800">
       <h1 className="font-bold text-2xl">react-simple-chat</h1>
-      <div>
-        {nickname ? (
-          <button onClick={clearNickname} className="flex items-center font-bold">
-            <img src="/assets/images/user.png" className="h-8 mr-1" />
-            <p>{nickname}</p>
-          </button>
-        ) : (
-          ""
-        )}
+      <div className="flex items-center">
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="outline" size="icon">
+              <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+              <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+              <span className="sr-only">Toggle theme</span>
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            <DropdownMenuItem className="cursor-pointer" onClick={() => setTheme("light")}>
+              Light
+            </DropdownMenuItem>
+            <DropdownMenuItem className="cursor-pointer" onClick={() => setTheme("dark")}>
+              Dark
+            </DropdownMenuItem>
+            <DropdownMenuItem className="cursor-pointer" onClick={() => setTheme("system")}>
+              System
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+        <div className="ml-2">
+          {nickname ? (
+            <button
+              onClick={clearNickname}
+              className="flex items-center font-bold"
+            >
+              <img src="/assets/images/user.png" className="h-8 mr-1" />
+              <p>{nickname}</p>
+            </button>
+          ) : (
+            ""
+          )}
+        </div>
       </div>
     </div>
   );
