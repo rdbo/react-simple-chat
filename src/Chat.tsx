cbcc0d257ba29973e3d0850cf6296a44759182ae
rdbo <rdbodev@gmail.com>
2024-04-26 08:33:20 -0300

    added basic message input handling

--- src/Chat.tsx
+++ src/Chat.tsx
@@ -1,8 +1,20 @@
-import { useState } from "react";
+import { KeyboardEvent, useState } from "react";
 import { Input } from "./components/ui/input";
+import { Textarea } from "./components/ui/textarea";
 
 export default function Chat() {
   const [message, setMessage] = useState("");
+
+  const sendMessage = () => {
+    setMessage("");
+  }
+
+  const handleKeyDown = (event: KeyboardEvent) => {
+    if (event.key == "Enter" && !event.shiftKey) {
+      event.preventDefault();
+      sendMessage();
+    }
+  }
 
   const updateMessage = (newMessage: string) => {
     setMessage(newMessage);
@@ -15,10 +27,11 @@ export default function Chat() {
         <p>hello</p>
       </div>
       <div className="pb-8 flex items-center">
-        <Input
+        <Textarea
           placeholder="Enter a message"
           className="px-2 py-2 rounded-md grow"
           value={message}
+          onKeyDown={handleKeyDown}
           onChange={(event) => updateMessage(event.target.value)}
         />
         <button className={"ml-2 rounded-full" + (message ? "" : " opacity-50")} disabled={!message}>
